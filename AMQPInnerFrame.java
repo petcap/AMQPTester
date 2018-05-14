23d26912f297b7854876039788722439ad8dc8c4
Peter Caprioli <petercap@kth.se>
2018-05-14 19:55:56 +0200

    Remove useless byte pop

--- AMQPInnerFrame.java
+++ AMQPInnerFrame.java
@@ -26,17 +26,6 @@ public class AMQPInnerFrame {
       ByteArrayBuffer amqpMethod = byteArrayBuffer.pop(2); //Pop 2 bytes
       //System.out.println("Assigning method: " + amqpMethod.toLong());
 
-      //Read first 4 octets from the arguments list
-      //Specified as an unsigned long, but the specification never mentions
-      //what this value should contain. RabbitMQ populates it with the argument
-      //length, but it could be pretty much anything
-      //The PHP library seems to be using some other interpretation of it, so
-      //we'll simply ignore it here because we already know the length anyway
-      ByteArrayBuffer uselessLong = byteArrayBuffer.pop(4);
-
-      System.out.println("Useless array long value:");
-      System.out.println(uselessLong.toHexString());
-
       //From now on, we need to start reading the method arguments
       //The structure of each argument list differs depending on which class
       //and method we're dealing with
